db705a0fdd42d52bcefc805a205dce6a08bef78f
Evan McCoy <evan.mccoy@gmail.com>
2019-04-18 11:02:36 -0400

    Built madlib generator function

---
+++ madlib_generator.py
@@ -0,0 +1,31 @@
+import random
+"""
+def caps_word(word):
+    word = str(word)
+    word = word.capitalize()
+    return word
+"""
+worst_food = input("What's your least favorite food? ")
+# worst_food = caps_word(worst_food)
+index = random.randint(1,5)
+
+def build_madlib(food, index):
+
+    if index == 1:
+        result = "It was the best of %s, it was the worst of %s" % (food, food)
+    elif index == 2:
+        result = ("Reach for the stars, for if you fall you'll land in the %s" % food)
+    elif index == 3:
+        result =  ("Call me %s" % food)
+    else:
+        result =  ("May the %s be with you" % food)
+    return result
+
+print(build_madlib(worst_food, index))
+
+"""
+if __name__ == '__main__':
+    madLib()
+"""
+
+
---
+++ madlib_imported.py
@@ -0,0 +1,5 @@
+import madlib_generator #import madlib_generator module
+import random
+
+print(madlib_generator.build_madlib("fruit", 2))
+
