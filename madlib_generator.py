253401b7d75a9767fefe25cef62937e00433062a
Evan McCoy <evan.mccoy@gmail.com>
2019-04-18 12:53:50 -0400

    broke into separate functios

--- madlib_generator.py
+++ madlib_generator.py
@@ -10,12 +10,11 @@ worst_food = input("What's your least favorite food? ")
 index = random.randint(1,5)
 
 def build_madlib(food, index):
-
     if index == 1:
         result = "It was the best of %s, it was the worst of %s" % (food, food)
     elif index == 2:
         result = ("Reach for the stars, for if you fall you'll land in the %s" % food)
-    elif index == 3:
+    elif index == 3:    
         result =  ("Call me %s" % food)
     else:
         result =  ("May the %s be with you" % food)
